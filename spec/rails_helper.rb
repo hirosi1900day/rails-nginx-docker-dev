8fefdd1d68cc19d3a9bfdbd4b9dac4e8b6006ec9
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-05 22:27:29 +0900

    wip

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ end
 # end
 
 Capybara.register_driver(:playwright) do |app|
-  channel = ENV['PLAYWRIGHT_CHROMIUM_CHANNEL'] || 'chrome'
+  channel = ENV['PLAYWRIGHT_CHROMIUM_CHANNEL'] || 'chromium'
   Capybara::Playwright::Driver.new(app, channel:, viewport: { width: 1400, height: 1400 }, acceptDownloads: true, headless: true, callback_on_save_trace: true)
 end
 Capybara.default_max_wait_time = 15
