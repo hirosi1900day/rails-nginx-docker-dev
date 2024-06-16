341b7eed585ed688678bf4042681522b2d73abbb
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-06-16 23:01:21 +0900

    wip

--- bin/rspec_results_merge_json.rb
+++ bin/rspec_results_merge_json.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# libディレクトリをロードパスに追加
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+
+# クラスを読み込み
 require 'merge_rspec_results'
 
-if $PROGRAM_NAME == __FILE__
-  input_dir = ARGV[0] || 'report'
-  output_file = ARGV[1] || 'tmp/rspec/merged_rspec_results.json'
-  merger = MergeRspecResults.new(input_dir:, output_file:)
-  merger.merge_files
-end
+input_dir = ARGV[0] || 'report'
+output_file = ARGV[1] || 'tmp/rspec/merged_rspec_results.json'
+merger = MergeRspecResults.new(input_dir:, output_file:)
+merger.merge_files
