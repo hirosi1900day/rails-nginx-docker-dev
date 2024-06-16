52b1b47209f8c761fcea5297934ce3cd12b4c102
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-06-16 22:53:34 +0900

    wip

--- bin/rspec_results_merge_json.rb
+++ bin/rspec_results_merge_json.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'merge_rspec_results'
+
 if $PROGRAM_NAME == __FILE__
   input_dir = ARGV[0] || 'report'
   output_file = ARGV[1] || 'tmp/rspec/merged_rspec_results.json'
-  merger = JSONMerger.new(input_dir: input_dir, output_file: output_file)
+  merger = MergeRspecResults.new(input_dir:, output_file:)
   merger.merge_files
 end
