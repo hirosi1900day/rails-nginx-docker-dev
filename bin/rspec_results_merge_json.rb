d74890049b89d57d688e0acd19fee99623a4ef52
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-06-16 22:51:37 +0900

    json結合をクラス化

--- bin/rspec_results_merge_json.rb
+++ bin/rspec_results_merge_json.rb
@@ -1,32 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'json'
-
-# マージするファイルのパスを取得
-file_paths = Dir.glob('report/*.json')
-
-# 最初のファイルを読み込む
-merged_data = JSON.parse(File.read(file_paths.shift))
-
-# 残りのファイルを順次マージ
-file_paths.each do |file_path|
-  data = JSON.parse(File.read(file_path))
-  
-  merged_data['examples'] += data['examples']
-  merged_data['summary']['duration'] += data['summary']['duration']
-  merged_data['summary']['example_count'] += data['summary']['example_count']
-  merged_data['summary']['failure_count'] += data['summary']['failure_count']
-  merged_data['summary']['pending_count'] += data['summary']['pending_count']
-  merged_data['summary']['errors_outside_of_examples_count'] += data['summary']['errors_outside_of_examples_count']
+if $PROGRAM_NAME == __FILE__
+  input_dir = ARGV[0] || 'report'
+  output_file = ARGV[1] || 'tmp/rspec/merged_rspec_results.json'
+  merger = JSONMerger.new(input_dir: input_dir, output_file: output_file)
+  merger.merge_files
 end
-
-# summary_line を更新
-merged_data['summary_line'] = "#{merged_data['summary']['example_count']} examples, #{merged_data['summary']['failure_count']} failures, #{merged_data['summary']['pending_count']} pending"
-
-# マージ結果をファイルに保存
-File.open('tmp/rspec/merged_rspec_results.json', 'w') do |file|
-  file.write(JSON.pretty_generate(merged_data))
-end
-
-puts 'Merged JSON files successfully!'
